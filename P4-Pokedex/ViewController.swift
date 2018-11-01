b503e79ea4d29eb10aa89b236c0c1df6c3b1378d
Adrian <adrian@Adrians-Mac.local>
2018-11-01 16:38:10 +0100

    With proper initialisers

--- P4-Pokedex/ViewController.swift
+++ P4-Pokedex/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var racename: String
+    var racename: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
